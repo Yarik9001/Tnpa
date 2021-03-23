7fc2256072ebe7bda56afd85b22ea5652e5a1a14
Andrew <lpmrfentazis@mail.ru>
2021-03-23 20:23:42 +0300

    Add sensor unit test
    FACKING SLAVES WRITE THE CODE FOR THE BOAT

--- UnitTests/TestBarom/TestBarom.ino
+++
@@ -1,8 +0,0 @@
-#define DepthPin A1
-
-  // Получаем и считаем значение датчика глубиры.
-  int raw = analogRead(DepthPin);
-//  float voltage = (float)raw * 5.0 / 1024.0; //тут не ровно 5 вольт, нужно смотреть как брать правильное напряжение....
-//  float  pressure_kpa = (voltage - 0.5) / 4.0 * 2400.0;
-//  float pressure_psi = pressure_kpa * 0.14503773773020923 / 14 + 1;
-//  float glubina = pressure_psi - 1;
--- UnitTests/TestSensors/TestSensors.ino
+++ UnitTests/TestSensors/TestSensors.ino
@@ -1,40 +1,22 @@
-#include <OneWire.h> // Для датчика температуры
-#include <DallasTemperature.h> // Для датчика температуры
-
 #define SerialSpeed 57600
 
-OneWire oneWire(OneWirePin);
-DallasTemperature sensors(&oneWire);
+#define DepthPin A1 // Пин для подключения датчика давления.
+#define TimeOutData 200 // Периодичность опроса датчиков
+#define Crutch -14 // Подгоняем значение
 
-int ArrData[4];
-
-void ReadData() {
-  // Получаем значение датчика влажности
-  ArrData[0] = analogRead(WaterPin);
-
-  // Получаем значение датчика температуры
-  sensors.requestTemperatures(); // опрашиваем датчики на OneWire(Температура);
-  ArrData[1] = sensors.getTempCByIndex(0);
-
-  // Получаем и считаем значение датчика глубиры.
-  int raw = analogRead(DepthPin);
-//  float voltage = (float)raw * 5.0 / 1024.0; //тут не ровно 5 вольт, нужно смотреть как брать правильное напряжение....
-//  float  pressure_kpa = (voltage - 0.5) / 4.0 * 2400.0;
-//  float pressure_psi = pressure_kpa * 0.14503773773020923 / 14 + 1;
-//  float glubina = pressure_psi - 1;
-  ArrData[2] = round(raw);
-}
-
+unsigned long LastTimeData; // таймер опроса датчиков
 
 void setup(){
     Serial.begin(SerialSpeed);
+    pinMode(DepthPin, INPUT);
+    Serial.println("start sensor test");
 }
 
 void loop(){
-    // Получем значения для двигателей
-    if ( millis() - LastTimeReceive >= TimeOutReceive ) {
-        ReceiveData(); // Получаем Значения для моторов
-        LastTimeReceive = millis(); // Время окончания работы
-    }
-    Serial.println();
+  
+  if ( millis() - LastTimeData >= TimeOutData ) {
+    Serial.print("Depth: ");
+    Serial.println( map( analogRead(DepthPin), 0, 1024, 0, 122 ) + Crutch);
+    LastTimeData = millis(); // Время окончания работы
+  }
 }
